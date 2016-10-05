2d7110061b6ef83772ec235c1c3abba3629f5f8b
Hugo López Fernández <hlfernandez@uvigo.es>
2016-10-05 14:26:48 +0100

    Use SING commons-csv library

--- s2p-core/src/main/java/es/uvigo/ei/sing/s2p/core/io/MaldiPlateLoader.java
+++ s2p-core/src/main/java/es/uvigo/ei/sing/s2p/core/io/MaldiPlateLoader.java
@@ -1,33 +1,43 @@
 package es.uvigo.ei.sing.s2p.core.io;
 
+import static es.uvigo.ei.sing.commons.csv.io.CsvReader.CsvReaderBuilder.newCsvReaderBuilder;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import es.uvigo.ei.sing.commons.csv.entities.CsvEntry;
+import es.uvigo.ei.sing.commons.csv.entities.CsvFormat;
+import es.uvigo.ei.sing.commons.csv.io.CsvReader;
+
 public class MaldiPlateLoader {
 	private static final String SEP = ",";
+	private static final CsvFormat FORMAT = new CsvFormat(SEP, '.', false, "\n");
 
 	public static Map<String, String> load(File file) throws IOException {
-		List<String> lines = Files.readAllLines(file.toPath());
-		return parseLines(lines);
+		return parseLines(getCsvReader().read(file));
 	}
 
-	private static Map<String, String> parseLines(List<String> lines) {
+	private static CsvReader getCsvReader() {
+		return newCsvReaderBuilder().withFormat(FORMAT).build();
+	}
+
+	private static Map<String, String> parseLines(List<CsvEntry> lines) {
 		Map<String, String> toret = new HashMap<String, String>();
-		String[] header = lines.get(0).split(SEP);
-		lines.subList(1, lines.size()).forEach(l -> {
-			String[] line = l.concat(" ").split(SEP);
-			String row = line[0];
-			for(int i = 1; i < line.length; i++) {
-				String current = line[i].trim();
-				if (!current.equals("")) {
-					toret.put(row + header[i], current);
+		CsvEntry header = lines.remove(0);
+		
+		lines.forEach(line -> {
+			String rowName = line.get(0);
+			for(int i = 1; i < line.size(); i++) {
+				String cellValue = line.get(i);
+				if (!cellValue.equals("")) {
+					toret.put(rowName + header.get(i), cellValue);
 				}
 			}
 		});
+		
 		return toret;
 	}
 }
