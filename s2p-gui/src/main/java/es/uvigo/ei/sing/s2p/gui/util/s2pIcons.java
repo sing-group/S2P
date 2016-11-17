792127f6f1ed6acb79e1ac260fbe53afa2a5a300
Hugo López Fernández <hlfernandez@uvigo.es>
2016-11-17 09:57:54 +0000

    Add S2P icons class to gui

---
+++ s2p-gui/src/main/java/es/uvigo/ei/sing/s2p/gui/util/s2pIcons.java
@@ -0,0 +1,9 @@
+package es.uvigo.ei.sing.s2p.gui.util;
+
+import javax.swing.ImageIcon;
+
+import es.uvigo.ei.sing.hlfernandez.ui.icons.Icons;
+
+public class s2pIcons {
+	public static final ImageIcon ICON_ABOUT = Icons.ICON_INFO_2_24; 
+}
